06e691f655f717409b366b95ebbefa927b63e47f
Rico Apon <rico.apon@hotmail.com>
2018-03-14 19:58:55 +0100

    Show error message on screen. Whenever the result is some kind of error, we show this to the user.

--- Angular/src/app/volunteer/add/volunteer.add.component.ts
+++ Angular/src/app/volunteer/add/volunteer.add.component.ts
@@ -6,6 +6,7 @@ import {GenderUtil} from "../../util/gender.util";
 import {VolunteerService} from "../../services/volunteer.service";
 import {ActivatedRoute, Router} from "@angular/router";
 import {SingleStringModel} from "../../valueobject/singlestring.model";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-volunteer-add',
@@ -13,6 +14,7 @@ import {SingleStringModel} from "../../valueobject/singlestring.model";
   styleUrls: ['./volunteer.add.component.css']
 })
 export class VolunteerAddComponent implements OnInit {
+  public errorMessage: string = null;
   public genders = GenderUtil.genders;
   public volunteer: VolunteerModel;
   public dateOfBirth;
@@ -57,6 +59,9 @@ export class VolunteerAddComponent implements OnInit {
     this.volunteerService.insertVolunteer(this.volunteer).subscribe(
       (volunteerExtId: SingleStringModel) => {
         this.router.navigate(['../' + volunteerExtId.value], {relativeTo: this.route});
+      },
+      (error: HttpErrorResponse) => {
+        this.errorMessage = error.error.title;
       }
     );
   }
