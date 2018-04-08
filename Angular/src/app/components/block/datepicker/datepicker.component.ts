ccfe1ff29ab150fd8af8d1a950e3fcc54902ceac
Rico Apon <rico.apon@hotmail.com>
2018-04-08 12:44:42 +0200

    Fix datepicker

--- Angular/src/app/components/block/datepicker/datepicker.component.ts
+++ Angular/src/app/components/block/datepicker/datepicker.component.ts
@@ -1,8 +1,12 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {ControlContainer, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-datepicker-component',
-  templateUrl: './datepicker.component.html'
+  templateUrl: './datepicker.component.html',
+  // Add viewProviders, so the input will be treated part of the form in the parent.
+  // https://stackoverflow.com/questions/39242219/angular2-nested-template-driven-form
+  viewProviders: [ { provide: ControlContainer, useExisting: NgForm } ]
 })
 export class DatepickerComponent {
   @Input()
@@ -15,7 +19,7 @@ export class DatepickerComponent {
   public dateChange : EventEmitter<string> = new EventEmitter<string>();
 
   @Input()
-  public required = true;
+  public required: boolean = true;
 
   @Input()
   public triedSubmit = false;
