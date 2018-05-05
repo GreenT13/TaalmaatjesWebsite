56396114fa083f4aee2bcdc6dc88ee6a888a6821
Rico Apon <rico.apon@hotmail.com>
2018-05-05 14:17:19 +0200

    Added VersionService test.

---
+++ RestApi/src/main/test/com/apon/service/TestVersionService.java
@@ -0,0 +1,24 @@
+package com.apon.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Test class for VersionService. There is no need to extend BaseTest, since there are no database functions.
+ */
+public class TestVersionService {
+    private VersionService versionService;
+
+    @Before
+    public void init() {
+        versionService = new VersionService();
+    }
+
+    @Test
+    public void getVersion() {
+        assertEquals(versionService.getVersion().getValue(), versionService.VERSION);
+        assertEquals(versionService.getSecuredVersion().getValue(), versionService.VERSION);
+    }
+}
